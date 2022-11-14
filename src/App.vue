54c6a5d6d908904dc3d332aed1370e8fbff1b14a
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-14 15:23:30 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+  }
+}
+
+</script>
+
+<template>
+
+</template>
+
