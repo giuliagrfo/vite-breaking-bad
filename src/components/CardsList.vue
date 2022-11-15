3c727578433e4f61d73d1800cd67bf05abba3a89
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-15 17:37:05 +0100

    add bonus - component results

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,11 +1,14 @@
 <script>
 import CardItem from './CardItem.vue';
+import ResultsItem from './ResultsItem.vue';
+
 import { store } from '../store.js'
 
 export default {
     name: 'CardsList',
     components: {
-        CardItem
+        CardItem,
+        ResultsItem
     },
     data() {
         return {
@@ -18,9 +21,9 @@ export default {
 <template>
     <div class="characters">
         <div class="container">
-            <div class="found_characters">
-                <h6>Found 62 characters</h6>
-            </div>
+
+            <ResultsItem />
+
             <div class="row list row-cols-5">
                 <CardItem :character="character" v-for="character in store.characters" />
             </div>
@@ -35,17 +38,6 @@ export default {
     background-color: $light;
     padding: 2rem;
     margin-top: 1rem;
-
-    .found_characters {
-        background-color: $dark;
-        padding: 1rem;
-        margin: 1rem 0;
-        color: $light;
-
-        h6 {
-            margin: 0;
-        }
-    }
 
     .list {
         justify-content: center;
---
+++ src/components/ResultsItem.vue
@@ -0,0 +1,40 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+    name: 'ResultsItem',
+    data() {
+        return {
+            store
+        }
+    },
+    computed: {
+        finalResult() {
+            return this.store.characters.length
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="results">
+        <p v-if="store.characters !== null">Results: {{ finalResult }}</p>
+    </div>
+
+</template>
+
+<style lang="scss">
+@use '../assets/scss/partials/variables' as*;
+
+.results {
+    background-color: $dark;
+    padding: 1rem;
+    margin: 1rem 0;
+    color: $light;
+
+    p {
+        margin: 0;
+    }
+}
+</style>
